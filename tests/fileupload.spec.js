73a8e4875bb8655c4bcc21305782dd19b3acf010
Aniket Shinde <aniketshinde12457@gmail.com>
2026-08-03 14:27:55 +0530

    File Uploader

---
+++ tests/fileupload.spec.js
@@ -0,0 +1,13 @@
+const {test, expect} = require('@playwright/test');
+const e = require('cors');
+
+test("File Upload Test", async function({page}) {
+
+    await page.goto("https://the-internet.herokuapp.com/upload");
+
+    await page.locator("#file-upload").setInputFiles("C:\\Users\\ACER\\Pictures\\18bae05e79ff7615f96566f77f401d6e.jpg");
+
+    await page.locator("#file-submit").click();
+
+    expect(await page.locator("//h3")).toHaveText("File Uploaded!");
+})
