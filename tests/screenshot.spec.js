ec0dae50b70e0cb3cbb66222721ca3ba35ce0f14
Aniket Shinde <aniketshinde12457@gmail.com>
2026-08-17 21:38:41 +0530

    Adding Screenshots and Tags

--- tests/screenshot.spec.js
+++ tests/screenshot.spec.js
@@ -6,13 +6,14 @@ test('Screenshot', async ({ page }) => {
     
 });
 
-test('Full Page ', async ({ page }) => {
+//we can also use the tags for run the specfic tags with help help of --grep "tagname"
+test('Full Page @full ', async ({ page }) => {
     await page.goto('https://www.opencart.com/index.php?route=cms/demo');
     await page.screenshot({path:'tests/screenshot/'+Date.now()+'fullpage.png',fullPage:true});
     
 });
 
-test('Specific Element', async ({ page }) => {
+test('Specific Element @element', async ({ page }) => {
     await page.goto('https://www.opencart.com/index.php?route=cms/demo');
     await page.locator("//a[@href='https://demo.opencart.com/']").screenshot({path:'tests/screenshot/'+Date.now()+'elementpage.png'});
     
